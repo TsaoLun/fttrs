f15792b6ccbce608160aacdf8eb0cbb1e9b0d619
tsaolun <ycl18@live.com>
2023-09-03 03:08:45 +0800

    init

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"golang.org/x/sync/errgroup"
+)
+
+type Config struct {
+	ParentAddr  string
+	ParentPort  string
+	ParentToken string
+	Name        string
+	BindPort    string
+	HostPort    string
+	ChildToken  string
+}
+
+var (
+	g      errgroup.Group
+	osType = "darwin" // linux darwin
+)
+
+func main() {
+	config := readConfig("./config.json")
+	if config.ParentAddr != "" {
+		createFrpcIni(&config)
+		execFrpc(&config)
+		app := fiber.New()
+		app.Use(logger.New())
+		app.Listen(fmt.Sprintf(":%s", config.HostPort))
+	}
+
+		createFrpsIni(&config)
+		execFrps(&config)
+
+	g.Wait()
+}
+
+func readConfig(path string) Config {
+	var config Config
+	file, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal(file, &config)
+	return config
+}
+
+func createFrpcIni(config *Config) {
+	file, err := os.Create("./frpc.ini")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	file.WriteString("[common]\n")
+	file.WriteString(fmt.Sprintf("server_addr = %s\n", config.ParentAddr))
+	file.WriteString(fmt.Sprintf("server_port = %s\n", config.ParentPort))
+	file.WriteString(fmt.Sprintf("token = %s\n", config.ParentToken))
+	file.WriteString("\n")
+	file.WriteString("[web]\n")
+	file.WriteString(fmt.Sprintf("type = http\n"))
+	file.WriteString(fmt.Sprintf("local_port = %s\n", config.BindPort))
+	file.WriteString(fmt.Sprintf("custom_domains = %s\n", "0.0.0.0:5500"))
+}
+
+func createFrpsIni(config *Config) {
+	file, err := os.Create("./frps.ini")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	file.WriteString("[common]\n")
+	file.WriteString(fmt.Sprintf("bind_port = %s\n", config.BindPort))
+	file.WriteString(fmt.Sprintf("token = %s\n", config.ChildToken))
+	file.WriteString(fmt.Sprintf("vhost_http_port = %s\n", config.HostPort))
+}
+
+func execFrpc(config *Config) {
+	cmd := exec.Command(fmt.Sprintf("./%s/frpc", osType), "-c", "./frpc.ini")
+	go func ()  {
+		<-make(chan os.Signal)
+		cmd.Process.Kill()
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		log.Printf("link success: %s parent addr %s port %s\n", config.Name, config.ParentAddr, config.ParentPort)
+	}();
+	go func() {
+		output, err := cmd.Output()
+		if err != nil {
+			log.Fatalf("exec frpc error: %s\n", err)
+		}
+		log.Printf("%s\n", output)
+		os.Exit(0)
+	}()
+}
+
+func execFrps(config *Config) {
+	cmd := exec.Command(fmt.Sprintf("./%s/frps", osType), "-c", "./frps.ini")
+	g.Go(func() error {
+		<-make(chan os.Signal)
+		cmd.Process.Kill()
+		return nil
+	})
+	go func() {
+		time.Sleep(2 * time.Second)
+		log.Printf("start frps success: %s bind %s host %s\n", config.Name, config.BindPort, config.HostPort)
+	}();
+	go func() {
+		output, err := cmd.Output()
+		if err != nil {
+			log.Fatalf("exec frps error: %s\n", err)
+		}
+		log.Printf("%s\n", output)
+		os.Exit(0)
+	}()
+}
